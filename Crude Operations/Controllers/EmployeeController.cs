c50832244ddf9d9a960e51af458218a180e16cc7
Patil <Patil@DESKTOP-H1FEUPU>
2023-05-03 12:00:48 +0530

    Add project files.

---
+++ Crude Operations/Controllers/EmployeeController.cs
@@ -0,0 +1,97 @@
+﻿using Crude_Operations.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crude_Operations.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeContext _dbContext;
+
+        public EmployeeController(EmployeeContext dbContext)
+        {
+            _dbContext = dbContext; 
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            if(_dbContext.Employees == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Employees.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            if (_dbContext.Employees == null)
+            {
+                return NotFound();
+            }
+            var employee = await _dbContext.Employees.FindAsync(id);
+            if(employee == null)
+            {
+                return NotFound();
+            }
+            return employee;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            _dbContext.Employees.Add(employee);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetEmployee), new { id = employee.ID }, employee);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutEmployee(int id, Employee employee)
+        {
+            if(id != employee.ID)
+            {
+                return BadRequest();
+            }
+            _dbContext.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            if(_dbContext.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _dbContext.Employees.FindAsync(id);
+            if(employee == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Employees.Remove(employee);
+
+            await _dbContext.SaveChangesAsync();
+
+            return Ok();
+                
+        }
+            
+
+
+    }
+}
---
+++ Crude Operations/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Crude_Operations.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
